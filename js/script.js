dc0918ca13bfe74ad6b3df413c815eadfd7ed642
Roberto <robertoanfuso98@gmail.com>
2021-06-18 12:19:06 +0200

    add colori

--- js/script.js
+++ js/script.js
@@ -97,12 +97,40 @@ const icons = [
     },
   ];
 
+const iconscolor = icons.map((element) =>{
+  const name=element.name;
+  const type=element.type;
+  const family=element.family;
+  const prefix=element.prefix;
 
-  icons.forEach((element)=> {
+  let color;
+  switch(type){
+    case 'user':
+      color="#0000FF";
+      break
+    case 'animal':
+      color="#FF0000";
+      break
+    case 'vegetable':
+      color="#008000";
+      break
+  }
+
+  return {
+    name,
+    type,
+    family,
+    prefix,
+    color
+  };
+});
+
+
+  iconscolor.forEach((element)=> {
     document.getElementById("icons").innerHTML +=`
     <div class="col">
      <div class="d-flex justify-content-center align-items-center flex-column p-5  bianco m-5 rounded">
-     <i class="${element.family} ${element.prefix}${element.name}"></i>
+     <i class="${element.family} ${element.prefix}${element.name}" style="color:${element.color}"></i>
        <div>${element.name}</div>
      </div>
     `
